684c45703d3c0c51da5ad9eb32fb52008364c3fb
Nawawi Jamili <nawawi@rutweb.com>
2020-03-17 05:00:57 +0800

    update: composer.json

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -28,11 +28,16 @@ class ComposerStaticInitab83e689f05eef1be66e4da087ba1a9b
         ),
     );
 
+    public static $classMap = array (
+        'MonoDB\\MonoDB' => __DIR__ . '/../..' . '/src/MonoDB.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInitab83e689f05eef1be66e4da087ba1a9b::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInitab83e689f05eef1be66e4da087ba1a9b::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInitab83e689f05eef1be66e4da087ba1a9b::$classMap;
 
         }, null, ClassLoader::class);
     }
